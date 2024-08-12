e6e80867bc1ed116698aa8a2805e0732fc957997
bryanpercy118 <bryanpercy77@gmail.com>
2024-08-12 11:01:06 -0500

    change

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class ProductsService {
+export class ProductsService extends PrismaClient implements OnModuleInit{
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
